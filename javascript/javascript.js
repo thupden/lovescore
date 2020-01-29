088f87f66c228ba88e4fc31dd735f30597ba8d22
thupden <47317869+thupden@users.noreply.github.com>
2020-01-29 21:52:35 +0530

    Add files via upload

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,20 +1,18 @@
-function myFunction()
-{
+function myFunction() {
   var x = document.getElementById("text").value;
-  if(x=="")
-  {
+  var y = document.getElementById("text1").value
+  if (x == "" || y == "") {
     alert("Name must be filled out");
     return false;
+  } else {
+    var num = Math.random();
+    var loveScore = Math.floor(num * 100);
+    document.getElementById("score").innerHTML = loveScore + '%';
+    document.getElementById("loveButton").innerHTML = "Re calculate love score";
+    document.getElementsByTagName("BUTTON")[0].setAttribute("onclick", "refreshPage()")
   }
-  else{
-  var num = Math.random();
-  var loveScore = Math.floor(num*100);
-  document.getElementById("score").innerHTML = loveScore + '%';
-  document.getElementById("loveButton").innerHTML = "Re calculate love score";
-  document.getElementsByTagName("BUTTON")[0].setAttribute("onclick","refreshPage()")
 }
-}
-function refreshPage()
-{
+
+function refreshPage() {
   location.reload(true);
 }
